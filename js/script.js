b0a74af66141b06809f36dec92d31c6fb01680ac
DavidKu <ts01561526@gmail.com>
2023-05-19 19:39:19 -0400

    update

--- js/script.js
+++ js/script.js
@@ -77,6 +77,60 @@ function hideSpinner() {
   document.querySelector(".spinner").classList.remove("show");
 }
 
+// Display Movie Details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split("=")[1]; // location.search can give us query stgins (after ?)
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  const div = document.createElement("div");
+  div.innerHTML = `
+        <div class="details-top">
+          <div>
+            <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+            />
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              8 / 10
+            </p>
+            <p class="text-muted">Release Date: XX/XX/XXXX</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              atque molestiae error debitis provident dolore hic odit, impedit
+              sint, voluptatum consectetur assumenda expedita perferendis
+              obcaecati veritatis voluptatibus. Voluptatum repellat suscipit,
+              quae molestiae cupiditate modi libero dolorem commodi obcaecati!
+              Ratione quia corporis recusandae delectus perspiciatis consequatur
+              ipsam. Cumque omnis ad recusandae.
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              <li>Genre 1</li>
+              <li>Genre 2</li>
+              <li>Genre 3</li>
+            </ul>
+            <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $1,000,000</li>
+            <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
+            <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
+            <li><span class="text-secondary">Status:</span> Released</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">Company 1, Company 2, Company 3</div>
+        </div>
+  `;
+  document.querySelector("#movie-details").appendChild(div);
+}
+
 // Fatch data from TMDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = "038bd02adfee6ead6956d09bd063ce20";
@@ -115,7 +169,7 @@ function init() {
       displayPopularShows();
       break;
     case "/movie-details.html":
-      console.log("Movie Details");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("TV Details");
