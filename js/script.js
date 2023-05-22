fc0ef2c0716aa24e6901a08a0a053760749a2bfc
tssytrack <ts01561526@gmail.com>
2023-05-22 16:04:15 -0400

    update

--- js/script.js
+++ js/script.js
@@ -168,6 +168,50 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// display slider movies
+async function displaySlider() {
+  const { results } = await fetchAPIData("movie/now_playing");
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+    </a>
+    <h4 class="swiper-rating">
+      <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+    </h4>
+    `;
+    document.querySelector(".swiper-wrapper").appendChild(div);
+  });
+
+  initSwiper();
+}
+
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnINteraction: true,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 // Display Show Details
 async function displayShowDetails() {
   const showId = window.location.search.split("=")[1]; // location.search can give us query stgins (after ?)
@@ -272,6 +316,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
+      displaySlider();
       displayPopularMovies();
       break;
     case "/shows.html":
